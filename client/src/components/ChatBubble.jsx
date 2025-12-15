36559c7931acd499c38bbef9a7a6752f5f652368
Zaid-maker <pzhafeez@gmail.com>
2025-12-16 02:03:21 +0500

    final fix

--- client/src/components/ChatBubble.jsx
+++ client/src/components/ChatBubble.jsx
@@ -43,7 +43,7 @@ const ChatBubble = ({
 
   const bubbleWidthClass = isEditing
     ? "w-full max-w-[min(720px,90vw)] min-w-[320px]"
-    : "max-w-[75%] min-w-[72px]";
+    : "max-w-[95%] min-w-[72px]";
 
   return (
     <div
